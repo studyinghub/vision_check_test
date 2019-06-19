116675e81ff69acda35f75ee6c61db40614dded2
Raymond Yoo <rayyoo4223@gmail.com>
2019-06-19 19:31:40 -0400

    fengshui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -46,18 +46,31 @@ class HomePage extends StatelessWidget {
                       colour: Color(0xFFFFFFFF),
                     ),
                   ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
                 ],
               ),
             ),
           ],
         ),
-        bottomNavigationBar: SizedBox(
-          child: Icon(
-            Icons.check,
-            size: 100.0,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.add),
+          onPressed: () {},
+        ),
+        bottomNavigationBar: new BottomAppBar(
+          color: Colors.green,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              new IconButton(
+                icon: new Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
       ),
