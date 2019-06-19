0e3c7a3360ef469c0b72e23ad8cdc4d278db25a2
Raymond Yoo <rayyoo4223@gmail.com>
2019-06-18 21:49:23 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(TestVisionCheck());
+
+class TestVisionCheck extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData().copyWith(
+        secondaryHeaderColor: Color(0xFF15C96C),
+        primaryColor: Color(0xFF15C96C),
+        //scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Vision Test')),
+          backgroundColor: Color(0xFF15C96C),
+        ),
+      ),
+    );
+  }
+}
