68cfe37258a49b098f04a47cbb06fb16595f850e
Dante Kim <motiv8lab@gmail.com>
2019-06-19 21:42:29 -0400

    Icons added

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -22,33 +22,43 @@ class CategoryList extends StatelessWidget {
         children: <Widget>[
           new Category(
             text: "Fitness",
+            iconData: Icons.fitness_center,
           ),
           new Category(
             text: "Spiritual",
+            iconData: Icons.mood,
           ),
           new Category(
             text: "Career",
+            iconData: Icons.next_week,
           ),
           new Category(
             text: "Relationships",
+            iconData: Icons.favorite,
           ),
           new Category(
             text: "Traveling",
+            iconData: Icons.flight,
           ),
           new Category(
             text: "Studying",
+            iconData: Icons.mode_edit,
           ),
           new Category(
             text: "Giving Back/Philanthropy",
+            iconData: Icons.nature_people,
           ),
           new Category(
             text: "Entrepreneurship",
+            iconData: Icons.lightbulb_outline,
           ),
           new Category(
             text: "Athletics",
+            iconData: Icons.directions_bike,
           ),
           new Category(
             text: "Other",
+            iconData: Icons.more_horiz,
           ),
         ],
       ),
@@ -57,8 +67,9 @@ class CategoryList extends StatelessWidget {
 }
 
 class Category extends StatelessWidget {
-  Category({@required this.text});
+  Category({@required this.text, @required this.iconData});
   final String text;
+  final IconData iconData;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -73,13 +84,19 @@ class Category extends StatelessWidget {
           ),
         ),
         child: FlatButton(
+          padding: EdgeInsets.all(30),
           color: Colors.white,
           onPressed: () {},
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
                 text,
                 style: TextStyle(fontSize: 15),
+              ),
+              Icon(
+                iconData,
+                size: 30,
               ),
             ],
           ),
